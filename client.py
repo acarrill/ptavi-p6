50d70e733718dba23abe1ef048ba12d89aa5dbcd
acarrill <alvarojcjc@hotmail.es>
2016-11-21 18:02:59 +0100

    s

--- client.py
+++ client.py
@@ -14,7 +14,7 @@ try:
     Method = sys.argv[1].upper()
     ReceiverLogin = sys.argv[2].split('@')[0]
     ReceiverIP = (sys.argv[2].split('@')[1]).split(':')[0]
-#Comprueba si el puerto es un digito antes de asignarlo
+# Comprueba si el puerto es un digito antes de asignarlo
     if not str.isdigit(sys.argv[2].split(':')[1]):
         raise IndexError
     ReceiverPort = int(sys.argv[2].split(':')[1])
@@ -27,20 +27,19 @@ if __name__ == "__main__":
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
         my_socket.connect((ReceiverIP, ReceiverPort))
         Message = (Method + ' sip:' + ReceiverLogin + '@' +
-                    ReceiverIP + ' SIP/2.0')
-                    
+                   ReceiverIP + ' SIP/2.0')
         print("Enviando:", Message)
         my_socket.send(bytes(Message, 'utf-8') + b'\r\n\r\n')
         data = my_socket.recv(1024)
         Answer = data.decode('utf-8')
-        OK = ('SIP/2.0 100 Trying\r\n\r\n' #Invite recibido correctamente
-                'SIP/2.0 180 Ring\r\n\r\n'
-                'SIP/2.0 200 OK\r\n\r\n')
+        OK = ('SIP/2.0 100 Trying\r\n\r\n'  # Invite recibido correctamente
+              'SIP/2.0 180 Ring\r\n\r\n'
+              'SIP/2.0 200 OK\r\n\r\n')
         if Answer == OK and Method == 'INVITE':
         	Method = 'ACK'
         	Message = (Method + ' sip:' + ReceiverLogin + '@' +
                     	ReceiverIP + ' SIP/2.0')
-        try: #Evitamos que se envien mensajes BYE innecesarios
+        try:  # Evitamos que se envien mensajes BYE innecesarios
             if Method == 'BYE':
                 raise KeyboardInterrupt
             my_socket.send(bytes(Message, 'utf-8') + b'\r\n\r\n')
